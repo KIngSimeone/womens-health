84ef8ce72a50b6494ee1958782ab0749d2cedb2a
KingSimeone <simeoneumoh@outlook.com>
2021-07-20 19:27:57 +0100

    asgi

--- womens_health/womens_health_config/asgi.py
+++ womens_health/womens_health_config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womens_health.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womens_health_config.settings')
 
 application = get_asgi_application()
