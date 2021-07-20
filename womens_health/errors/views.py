8ba4589e4498a9904c4eafb2c672ae66b331a80f
KingSimeone <simeoneumoh@outlook.com>
2021-07-20 20:54:23 +0100

    getError

--- womens_health/errors/views.py
+++ womens_health/errors/views.py
@@ -8,3 +8,15 @@ class ErrorCodes(IntEnum):
     UNAUTHENTICATED_REQUEST = 1
     UNAUTHORIZED_REQUEST = 2
     MISSING_FIELDS = 3
+
+
+# base error
+def getError(code, defaultMessage):
+    try:
+        _, _ = Error.objects.get_or_create(
+            code=code, description=defaultMessage)
+
+        return {'errorCode': code, 'message': defaultMessage}
+
+    except IntegrityError:
+        return {'errorCode': code, 'message': defaultMessage}
