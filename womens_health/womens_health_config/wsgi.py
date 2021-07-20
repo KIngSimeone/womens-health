cefc674987cdef2331f6cf33de8761a269b3ff1c
KingSimeone <simeoneumoh@outlook.com>
2021-07-20 19:33:35 +0100

    wsgi

--- womens_health/womens_health_config/wsgi.py
+++ womens_health/womens_health_config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womens_health.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'womens_health_config.settings')
 
 application = get_wsgi_application()
