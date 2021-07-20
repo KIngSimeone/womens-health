e5cf8904cba8ca83a8928adaea45234908484cc2
KingSimeone <simeoneumoh@outlook.com>
2021-07-20 20:18:18 +0100

    updators

--- womens_health/api_utils/views.py
+++ womens_health/api_utils/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from http import HTTPStatus
 
-# Create your views here.
+
+def badRequestResponse(data):
+    return errorResponse(HTTPStatus.BAD_REQUEST, data)
+
+
+def unAuthorizedResponse(data):
+    return errorResponse(HTTPStatus.FORBIDDEN, data)
+
+
+def unAuthenticatedResponse(data):
+    return errorResponse(HTTPStatus.UNAUTHORIZED, data)
+
+
+def resourceConflictResponse(data):
+    return errorResponse(HTTPStatus.CONFLICT, data)
+
+
+def resourceNotFoundResponse(data):
+    return errorResponse(HTTPStatus.NOT_FOUND, data)
+
+
+def internalServerErrorResponse(data):
+    return errorResponse(HTTPStatus.INTERNAL_SERVER_ERROR, data)
+
+
+def errorResponse(httpStatusCode, data):
+    return JsonResponse(data, status=httpStatusCode, safe=False)
+
+
+# success responses
+def createdResponse(message="", body={}):
+    return successResponse(HTTPStatus.CREATED, message=message, body=body)
+
+
+def paginatedResponse(httpStatusCode=HTTPStatus.OK, message="", body={}, pagination={}, **kwargs):
+    return successResponse(httpStatusCode, message, body, pagination, kwargs)
+
+
+def successResponse(httpStatusCode=HTTPStatus.OK, message="", body={}, pagination=None, kwargs=None):
+    responseData = dict()
+    responseData['data'] = body
+    responseData['metaData'] = kwargs
+    responseData['message'] = message
+
+    if pagination:
+        responseData['pagination'] = pagination
+
+    response = JsonResponse(responseData, status=httpStatusCode, safe=False)
+
+    return response
