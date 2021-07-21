66f9999e62c3e8e6743a50b9cb6c2f8e34f3db3b
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 08:23:20 +0100

    views setup

--- womens_health/Users/views.py
+++ womens_health/Users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+import json
+import logging
+from errors.views import ErrorCodes
+from api_utils.views import (
+    badRequestResponse, request_response, resourceConflictResponse, internalServerErrorResponse,
+    unAuthenticatedResponse,
+    unAuthorizedResponse, successResponse, resourceNotFoundResponse,
+)
+from api_utils.validators import (
+    validateKeys
+)
+from .utils import (
+    createPatient
+)
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
