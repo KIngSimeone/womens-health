01acbeed0429b0762922821624686600bcf23d49
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 08:51:06 +0100

    validateInputFormat

--- womens_health/api_utils/validators.py
+++ womens_health/api_utils/validators.py
@@ -9,3 +9,15 @@ def validateKeys(payload, requiredKeys):
             missingKeys.append(key)
 
     return missingKeys
+
+def validateInputFormat(inputList, email, phone):
+    if not validate_input_list_is_empty_and_clean(inputList):
+        return False, f"Name can neither contain special characters nor be empty:{inputList}"
+
+    if not validateEmailFormat(email):
+        return False, f"Email format is invalid: {email}"
+
+    if not validatePhoneFormat(phone):
+        return False, f"Phone Format is Invalid: {phone}"
+
+    return True, "success"
