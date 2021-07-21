78d5bfec972e6a2827031a967e5e0dbacd96d2f6
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 09:30:44 +0100

    toUiReadableDateFormat

--- womens_health/data_transformer/views.py
+++ womens_health/data_transformer/views.py
@@ -1,4 +1,11 @@
+import logging
 from datetime import datetime
+
+import pytz
+from django.utils import timezone
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def dateIsISO(value):
@@ -9,3 +16,16 @@ def dateIsISO(value):
         return True
     except ValueError:
         return False
+
+
+def toUiReadableDateFormat(value):
+    try:
+        if value is None:
+            return ""
+
+        localizedValue = timezone.localtime(
+            value, pytz.timezone('UTC'))
+        return datetime.strftime(localizedValue, "%b %d, %Y %I:%M%p")
+    except Exception as ex:
+        logger.error(ex)
+        return str(value)
