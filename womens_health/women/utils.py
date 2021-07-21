7f08c1cf3e24f89ed3b3576b4fea90b5b0add29f
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 22:00:02 +0100

    updatePeriodInfo

--- womens_health/women/utils.py
+++ womens_health/women/utils.py
@@ -22,3 +22,18 @@ def createPeriodInfo(patient, cycle_average, period_average):
             "createPeriodInfo@Error :: Error occurred while creating the period information")
         logger.error(e)
         return None, str(e)
+
+def updatePeriodInfo(periodInfo, cycle_average, period_average):
+    """updating period info"""
+    try:
+        periodInfo.cycle_average = cycle_average
+        periodInfo.period_average = period_average
+        periodInfo.save()
+
+        return periodInfo, "success"
+    
+    except Exception as e:
+        logger.error(
+            "updatePeriodInfo@Error :: Error occurred while updating the period information")
+        logger.error(e)
+        return None, str(e)
