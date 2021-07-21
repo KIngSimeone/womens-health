51b36965fcb8a3b383a5e8859f072ae6c42d3b34
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 21:39:47 +0100

    PeriodInfo models

---
+++ womens_health/women/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from Users.models import Patient
+
+class PeriodInfo(models.Model):
+    id = models.AutoField(primary_key=True, editable=False, unique=True)
+    cycle_average = models.TextField(null=True)
+    period_average = models.TextField(null=True)
+    patient = models.ForeignKey(
+        Patient, on_delete=models.SET_NULL, null=True)
+
+    created_at = models.DateTimeField("created_at", auto_now_add=True)
+    updated_at = models.DateTimeField("updated_at", auto_now=True)
