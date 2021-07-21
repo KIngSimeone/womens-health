e5ffa5c8e523918c43e49f32d0f214e69eed32c6
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 08:20:24 +0100

    createPatient

--- womens_health/Users/utils.py
+++ womens_health/Users/utils.py
@@ -1,14 +1,41 @@
-import uuid
+import logging
 import secrets
-from django.conf import settings
-from django.contrib.auth.hashers import make_password, check_password
-from .models import Patient
+import uuid
 from datetime import timedelta
+
+from django.conf import settings
+from django.contrib.auth.hashers import check_password, make_password
 from django.utils import timezone
+
+from .models import Patient
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def getExpiresAt(minutes=None):
     if not minutes:
         return (timezone.now() + timedelta(minutes=eval(settings.DURATION)))
-    
+
     return (timezone.now() + timedelta(minutes=minutes))
+
+
+def createPatient(firstname, lastname, email, phone, password, birthday):
+    """creating new patient"""
+    try:
+        patient = Patient.objects.create(
+            id=int(str(uuid.uuid4().int)[::6]),
+            first_name=firstname,
+            last_name=lastname,
+            phone=phone,
+            email=email,
+            password=make_password(password),
+            birthday=birthday
+        )
+        return patient, "success"
+
+    except Exception as e:
+        logger.error(
+            "createPatient@Error :: Error occurred while creating the patient")
+        logger.error(e)
+        return None, str(e)
