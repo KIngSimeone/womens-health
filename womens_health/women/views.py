1736e74acc8f89c0cdd14ff27ac1ea31a53236ca
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 22:14:42 +0100

    updates

--- womens_health/women/views.py
+++ womens_health/women/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
+import logging
+from django.conf import settings
+from errors.views import ErrorCodes
+from api_utils.views import (
+    badRequestResponse, resourceConflictResponse, internalServerErrorResponse,
+    unAuthenticatedResponse, requestResponse,
+    unAuthorizedResponse, successResponse, resourceNotFoundResponse
+)
+from Users.utils import getUserByAccessToken
+from data_transformer.views import dateIsISO
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
-# Create your views here.
+def createCycles(request):
+    """create cycle api endpoint"""
+    body = json.loads(request.body)
+
+    token = request.headers.get('Token')
+    if token is None:
+        return requestResponse(badRequestResponse, ErrorCodes.INVALID_CREDENTIALS,
+                                "Token is missing in the request headers")
+
+    # get user with access token
+    user = getUserByAccessToken(token)
+    if user is None:
+        return requestResponse(unAuthenticatedResponse, ErrorCodes.UNAUTHENTICATED_REQUEST,
+                                "Your session has expired. Please login.")
