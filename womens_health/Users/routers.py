cbcd3be69713732afa0dc4158abe446dd3495cad
KingSimeone <simeoneumoh@outlook.com>
2021-07-21 09:40:10 +0100

    patient_router

---
+++ womens_health/Users/routers.py
@@ -0,0 +1,11 @@
+from .views import provisionPatient
+from errors.views import ErrorCodes
+from api_utils.views import badRequestResponse, requestResponse
+
+def patient_router(request):
+    if request.method == 'POST':
+        return provisionPatient(request)
+    else:
+        return requestResponse(badRequestResponse, 
+                              ErrorCodes.GENERIC_ERROR,
+                              "Invalid Request method, Request Method must be 'POST'.")
