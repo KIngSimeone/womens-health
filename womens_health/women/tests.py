9e6ce56ff718b796d9d8dbf6ba0e679718331a09
KingSimeone <simeoneumoh@outlook.com>
2021-07-22 14:42:15 +0100

    updates

--- womens_health/women/tests.py
+++ womens_health/women/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.conf import settings
+from django.test import Client, TestCase
 
 # Create your tests here.
+client = Client()
+secret = settings.ROOT_SECRET
+content_type = "application/json"
