497ca9e1de9ccf643fb5834aae9444fc44ee1511
KingSimeone <simeoneumoh@outlook.com>
2021-07-22 06:57:59 +0100

    cycle_event_router

--- womens_health/women/routers.py
+++ womens_health/women/routers.py
@@ -1,4 +1,7 @@
-from .views import createCycles
+from .views import createCycles, cycleEvent
 
 def cycle_router(request):
     return createCycles(request)
+
+def cycle_event_router(request):
+    return cycleEvent(request)
